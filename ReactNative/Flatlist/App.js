52b1847cf620451bb646b51d55c53b4fae23f1c8
Jose Zapata <a1602411@myy.haaga-helia.fi>
2018-02-12 16:39:51 +0200

    T3 upload

---
+++ ReactNative/Flatlist/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, Text, View,Button,TextInput,FlatList } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputTxt : "",
+      data : []
+    }
+  }
+
+  buttonPressed = () => {
+    this.setState({
+      //set new state and clear inputTxt
+      data : [...this.state.data,{key: this.state.inputTxt}],inputTxt : ''}
+    );
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput onChangeText={ (text) => this.setState({inputTxt : text})}></TextInput>
+        <Button onPress={this.buttonPressed} title="click"></Button>
+        <View>
+          <FlatList data={this.state.data}renderItem={({item}) =><Text>{item.key}</Text>} /> 
+        </View>
+        
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    flexDirection:'column'
+  },
+  cItems : {
+    width: 50,
+    height: 50
+  },
+});
--- ReactNative/T03/App.js
+++ ReactNative/T03/App.js
@@ -1,31 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View,Button,TextInput,FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert,FlatList } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputTxt : "",
-      data : []
-    }
+  
+    this.state = {n1: 0, n2: 0, total: 0,data: []};
   }
 
-  buttonPressed = () => {
-    this.setState({
-      data : [...this.state.data,{key: this.state.inputTxt},text : '']
-    });
+  buttonAdd = () => {
+    const num1 = this.state.n1
+    const num2 = this.state.n2
+    //math operation
+    let total = parseInt(num1)  + parseInt(num2);
+    //txt
+    const totalTxt = `${num1.toString()} + ${num2.toString()} = ${total.toString()}`
+    //Alert.alert(totalTxt)
+    this.setState( 
+      { total : total,
+       data : [...this.state.data,{key: totalTxt},],n1: '', n2: '',
+     }
+   );
+  }
+
+  buttonDelete = () => {
+    const num1 = this.state.n1
+    const num2 = this.state.n2
+    let total = parseInt(num1)  - parseInt(num2)
+    const totalTxt = `${num1.toString()} - ${num2.toString()} = ${total.toString()}`
+
+    this.setState( 
+       { total : total,
+        data : [...this.state.data,{key: totalTxt}],n1: '', n2: '',
+      }
+    );
+
+    
+  }
+
+  clear = () => {
+
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput onChangeText={ (text) => this.setState({inputTxt : text})}></TextInput>
-        <Button onPress={this.buttonPressed} title="click"></Button>
-        <View>
+        <Text style={styles.txtOut}>Result {this.state.total} </Text>
+        <Text >N1 : {this.state.n1} </Text>
+        <Text>N2 : {this.state.n2} </Text>
+        <TextInput  style={styles.textbox}  keyboardType='numeric' onChangeText={ (n1) => this.setState( {n1}) } value={this.state.n1.toString()} />
+        <TextInput  style={styles.textbox}  keyboardType='numeric'  onChangeText={ (n2) => this.setState( {n2}) } value={this.state.n2.toString()} />
+        <Button title="+" color="#FF0000" onPress={this.buttonAdd} />
+        <Button title="-" color="#000000" onPress={this.buttonDelete}/>
+        <View >
+          <Text>History</Text>
           <FlatList data={this.state.data}renderItem={({item}) =><Text>{item.key}</Text>} /> 
         </View>
-        
-
       </View>
     );
   }
@@ -34,12 +64,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
-    justifyContent: 'center',
-    flexDirection:'column'
+    backgroundColor: '#fff',
+    flexDirection:'column',
+    justifyContent:  "flex-start",
+    paddingVertical : 30
   },
-  cItems : {
-    width: 50,
-    height: 50
+
+  textbox : {
+    borderWidth: 1,
+    padding : 12
   },
+  txtOut : {
+    fontSize :50,
+  },
+
 });
