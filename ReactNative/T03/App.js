f3140652d43ac815b3d40361ce66b7579ccc234d
Jose Zapata <a1602411@myy.haaga-helia.fi>
2018-02-12 12:45:53 +0200

    t03 progress

--- ReactNative/T03/App.js
+++ ReactNative/T03/App.js
@@ -10,14 +10,22 @@ export default class App extends React.Component {
     }
   }
 
+  buttonPressed = () => {
+    this.setState({
+      data : [...this.state.data,{key: this.state.inputTxt},text : '']
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <TextInput onChangeText={ (text) => this.setState({inputTxt : text})}></TextInput>
-        <Button title="click"></Button>
+        <Button onPress={this.buttonPressed} title="click"></Button>
         <View>
-        <FlatList data={this .state.data} renderItem={({item}) =><Text>{item.key}   </ Text>}/>
+          <FlatList data={this.state.data}renderItem={({item}) =><Text>{item.key}</Text>} /> 
         </View>
+        
+
       </View>
     );
   }
@@ -26,8 +34,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'green',
     justifyContent: 'center',
+    flexDirection:'column'
+  },
+  cItems : {
+    width: 50,
+    height: 50
   },
 });
